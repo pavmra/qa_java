99391e7124c5aaa18fca9c38d8cbabb0a7c4c54a
Pavel Navoev <pavmra@mail.ru>
2025-05-26 21:02:41 +0300

    4

--- src/test/java/tests/TestFeline.java
+++ src/test/java/tests/TestFeline.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import java.util.List;
-import static org.testng.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestFeline {
 
